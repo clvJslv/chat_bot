75dc6565ed3af29c753348ae428b0abad14c15f6
Ivanjr8 <ivanjr8@hotmail.com>
2025-08-26 16:44:53 -0300

    1
    1

--- pages/db_connection.py
+++ pages/db_connection.py
@@ -2,6 +2,51 @@
 import pyodbc
 import streamlit as st
 
+with open("assets/style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    
+    # Estilização da barra lateral
+st.markdown("""
+    <style>
+        [data-testid="stSidebar"] {
+            background-color: #1f2937;
+            color: white;
+        }
+        [data-testid="stSidebar"] h2 {
+            color: #10b981;
+        }
+        [data-testid="stSidebar"] .stButton button {
+            background-color: #10b981;
+            color: white;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Barra lateral personalizada
+with st.sidebar:
+    st.markdown("## 🧭 Navegação")
+    if st.button("🤖 Ir para Chatbot"):
+        st.switch_page("pages/chatbot.py")
+    if st.button("🤖 Ir para Cadastrar_Questões"):
+        st.switch_page("pages/Cadastrar_Questões.py")
+    if st.button("🤖 Ir para Gerar_Simulado"):
+        st.switch_page("pages/Gerar_Simulado.py")
+    if st.button("🤖 Ir para conn_azure"):
+        st.switch_page("pages/conn_azure.py")
+    
+    if st.button("🤖 Retornar"):
+        st.switch_page("gemini.py")
+    
+
+    st.markdown("---")
+    st.markdown("## ⚙️ Configurações")
+    st.selectbox("Modo de exibição", ["Claro", "Escuro", "Automático"])
+    st.slider("Sensibilidade do modelo", 0.0, 1.0, 0.5)
+
+    st.markdown("---")
+    st.markdown("### 📞 Suporte")
+    st.write("Email: suporte@meuapp.com")
+
 # Conexão com a base de dados
 class DatabaseConnection:
     def __init__(self):
