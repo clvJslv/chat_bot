78b0284a0e7f21849cc6b2ab8a274bddb2462aea
Ivanjr8 <ivanjr8@hotmail.com>
2025-08-26 19:41:26 -0300

    1
    1

--- gemini.py
+++ gemini.py
@@ -42,7 +42,7 @@ with st.sidebar:
         st.switch_page("pages/Gerar_Simulado.py")
     if st.button("🤖 Ir para conn_azure", key="btn_azure"):
         st.switch_page("pages/conn_azure.py")
-    if st.button("🤖 Retornar", key="btn_teste"):
+    if st.button("🤖 teste_con", key="btn_teste"):
         st.switch_page("teste_con.py")
     if st.button("🤖 Retornar", key="btn_retornar"):
         st.switch_page("gemini.py")
