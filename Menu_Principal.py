60357a87693ae42f7c151b016ca712b33d6cf922
Ivanjr8 <ivanjr8@hotmail.com>
2025-08-25 23:59:29 -0300

    1
    1

---
+++ Menu_Principal.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Idéias YOLO v8",
+    page_icon="🔮",
+    layout="wide",  # "centered",  ou "wide"
+    initial_sidebar_state="expanded",  # "auto", "expanded" ou "collapsed"
+
+)
+
+st.write("# Aplicativos de Detecção de Faces e Objetos 🔮")
+
+st.markdown(
+    """
+    **Ajuda:** [https://docs.streamlit.io/](https://docs.streamlit.io/)
+
+    **Reportar falha:** [https://github.com/streamlit/streamlit/issues](https://github.com/streamlit/streamlit/issues) 
+
+    **Descrição:** Portal de Aplicativos de Rede Neural baseados em YOLO.
+"""
+)
