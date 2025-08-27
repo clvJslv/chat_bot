131772822f67f07314893e7462e892365b6d21df
Ivanjr8 <ivanjr8@hotmail.com>
2025-08-26 23:14:49 -0300

    1
    1

--- gemini.py
+++ gemini.py
@@ -31,7 +31,7 @@ st.markdown("""
             color: #10b981;
         }
         [data-testid="stSidebar"] .stButton button {
-           background-color: #00000024;
+           background-color: #000000a9;
            color: rgba(245, 245, 245, 0.849);
            border: none;
            border-radius: 8px;
